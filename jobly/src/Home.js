84d3d653178aac0b780ea514ca27d770f53dc44b
kelly wolosonovich <k.wolosonovich@gmail.com>
2021-01-24 13:00:55 -0700

    added materialUI; created home component

---
+++ jobly/src/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Card, CardContent, CardHeader } from "@material-ui/core";
+
+
+function Home() {
+
+return (
+  <section className="home">
+    <Card>
+      <CardContent>
+        <CardHeader />
+            Jobly
+      </CardContent>
+    </Card>
+  </section>
+);
+}
+
+export default Home
